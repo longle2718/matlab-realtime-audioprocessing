0f8f4cd3403c46cb0b06279c62b9084f844dfbe0
Long Le <longle1@illinois.edu>
2014-03-01 23:01:10 -0600

    remove fillers model from the framework to focus on keyword recognition

--- trainKWR.m
+++ trainKWR.m
@@ -16,36 +16,17 @@ fs = 8e3;
 win = 13e-3*fs; % == nfft unless explicitly specified 
 inc = 10e-3*fs;
 
-fnames = dir('speech/train/filler/*.mp3');
-numfids = length(fnames);
-filler = cell(1,numfids);
-for k = 1:numfids
-    tmp = ['speech/train/filler/' fnames(k).name];
-	[filler{k}, tmpFs] = audioread(tmp);
-    filler{k} = filler{k}(:,1);
-	filler{k} = resample(filler{k}, fs, tmpFs);
-end
-
-fnames = dir('speech/train/keyword/*.mp3');
+fnames = dir('speech/train/*.mp3');
 numfids = length(fnames);
 keyword = cell(1,numfids);
 for k = 1:numfids
-    tmp = ['speech/train/keyword/' fnames(k).name];
+    tmp = ['speech/train/' fnames(k).name];
 	[keyword{k}, tmpFs] = audioread(tmp);
     keyword{k} = keyword{k}(:,1);
 	keyword{k} = resample(keyword{k}, fs, tmpFs);
 end
 
-%speech = cell2mat([filler'; keyword']);
-
 %% Feature extraction
-nFiller = numel(filler);
-fillerC = cell(nFiller, 1);
-for k = 1:nFiller
-    % 12 mel cepstrum coefficients and first order derivative, i.e. delta
-	fillerC{k} = melcepst(filler{k}, fs, 'd', 12, floor(3*log(fs)), win, inc);
-end
-
 nKeyword = numel(keyword);
 keywordC = cell(nKeyword, 1);
 for k = 1:nKeyword
