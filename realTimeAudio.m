186ca66fc3b6d31eab003b048656c9ecb62ceb50
Long Le <longle1@illinois.edu>
2014-05-02 12:47:48 -0500

    dynamically adjusting the magnitude scale of the spectrum

--- realTimeAudio.m
+++ realTimeAudio.m
@@ -40,7 +40,7 @@ while(~done)
         dataDFT = dataDFT(1:N/2+1);
         freq = 0:fs/N:fs/2;
         plot(freq, abs(dataDFT)); grid on
-        axis([0 fs/2 0 0.1]);
+        axis([0 fs/2 0 1/N]);
         xlabel('Frequency (Hz)'); ylabel('Magnitude')
         drawnow;
     end
